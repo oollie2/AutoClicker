4b1cef8909feccdd3119d3285221f80487fd1aad
oollie2 <olliesharratt34@gmail.com>
2022-04-15 03:10:05 +0100

    #2 app can now detect a new release, then can prompt, download and run the installer

--- src/AutoClicker.Application/App.xaml.cs
+++ src/AutoClicker.Application/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoClicker.Classes;
+using System;
 using System.IO;
 using System.Windows;
 
@@ -9,13 +10,29 @@ namespace AutoClicker
     /// </summary>
     public partial class App : Application
     {
-        private Classes.Logging Logger;
-        private void Application_Startup(object sender, StartupEventArgs e)
+        private Logging Logger;
+        private async void Application_Startup(object sender, StartupEventArgs e)
         {
             Logger = new();
             Logger.ExceptionLocation = Environment.ExpandEnvironmentVariables(@"%APPDATA%\oollie34\AutoClicker\Logs\");
             Logger.ExceptionLogging = true;
             Logger.DeleteOld();
+
+            Updater updater = new();
+            await updater.CheckVersionsAsync();
+            if(updater.State == UpdateState.UpdateAvailable)
+            {
+                MessageBoxResult update = MessageBox.Show("A new update is available, would you like to download and install?",
+                    "Update Check",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question);
+                if(update == MessageBoxResult.Yes)
+                {
+                    updater.PerformUpdate();
+                    return;
+                }
+            }
+            File.Delete(updater.DownloadLocation);
             new MainWindow().Show();
         }
 
---
+++ src/AutoClicker.Application/Classes/Updater.cs
@@ -0,0 +1,93 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AutoClicker.Classes
+{
+    internal class Updater
+    {
+        private GitHubClient client;
+        private IReadOnlyList<Release> releases;
+        private readonly string OwnerName = "oollie34";
+        private readonly string RepoName = "AutoClicker";
+        public string DownloadLocation;
+        public UpdateState State { get; set; }
+        public Updater()
+        {
+            DownloadLocation = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Downloads\AutoClicker.msi");
+        }
+        public async Task CheckVersionsAsync()
+        {
+            client = new(new ProductHeaderValue("AutoClickerUpdate"));
+            releases = await client.Repository.Release.GetAll(OwnerName, RepoName);
+            Version latestGitHubVersion = new(releases[0].TagName);
+            Version localVersion = new(Assembly.GetEntryAssembly().GetName().Version.Major + "." +
+                Assembly.GetEntryAssembly().GetName().Version.Minor + "." + Assembly.GetEntryAssembly().GetName().Version.Build);
+            int versionComparison = localVersion.CompareTo(latestGitHubVersion);
+            Debug.WriteLine(versionComparison);
+            if (versionComparison < 0)
+            {
+                State = UpdateState.UpdateAvailable;
+            }
+            else
+            {
+                State = UpdateState.NoUpdateAvailable;
+            }
+        }
+        public async void PerformUpdate()
+        {
+            await PerformUpdateAsync();
+        }
+        private async Task PerformUpdateAsync()
+        {
+            //todo we need a progress bar for the download
+            var assets = await client.Repository.Release.GetAllAssets(OwnerName, RepoName, releases[0].Id);
+            HttpClient httpClient = new();
+            HttpResponseMessage response = await httpClient.GetAsync(assets[0].BrowserDownloadUrl);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                using Stream stream = await response.Content.ReadAsStreamAsync();
+                Debug.WriteLine(DownloadLocation);
+                FileInfo fileInfo = new(DownloadLocation);
+                using FileStream fileStream = fileInfo.OpenWrite();
+                await stream.CopyToAsync(fileStream);
+                RunUpdate();
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Unable to download the update file.\r\nPlease raise an issue with the log file and any other details on GitHub.",
+                    "Unable to Update",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                if (result == MessageBoxResult.OK)
+                    Environment.Exit(0);
+            }
+        }
+        private void RunUpdate()
+        {
+            using Process installerProcess = new();
+            ProcessStartInfo processInfo = new();
+            processInfo.FileName = "cmd.exe";
+            Debug.WriteLine(DownloadLocation);
+            processInfo.Arguments = "/C start " + DownloadLocation;
+            processInfo.UseShellExecute = false;
+            processInfo.CreateNoWindow = false;
+            installerProcess.StartInfo = processInfo;
+            installerProcess.Start();
+            Environment.Exit(0);
+        }
+    }
+    public enum UpdateState
+    {
+        NotChecked,
+        UpdateAvailable,
+        NoUpdateAvailable
+    }
+}
